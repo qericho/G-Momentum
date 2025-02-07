3612fac164ae0f5b6b920b93d5d2f40fa3ab0de9
no_name <echstmr@gmail.com>
2025-02-08 07:53:40 +0800

    Update Weather.jsx

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react"
 const Weather = () => {
     const [data, setData] = useState(null);
     const API_KEY = import.meta.env.VITE_API_KEY;
-    const city = 'Gatid';
+    const city = 'philippines';
     
     useEffect(() => {
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
